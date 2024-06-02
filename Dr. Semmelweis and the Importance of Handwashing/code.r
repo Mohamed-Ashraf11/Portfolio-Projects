4bc9a46c121180cc0fd12d8a76b0fcf5665911bd
Mohamed A. Youssef <mohamed.youssef@ejust.edu.eg>
2024-06-02 20:27:40 +0300

    Update code.r

--- Dr. Semmelweis and the Importance of Handwashing/code.r
+++ Dr. Semmelweis and the Importance of Handwashing/code.r
@@ -1,1 +1,50 @@
+#loading the required packages
+library(dplyr)
+library(ggplot2)
+
+
+# reading and viewing data
+
+yearly = read.csv("yearly_deaths_by_clinic.csv", sep = ",", header =  TRUE)
+View(yearly)
+monthly = read.csv("monthly_deaths.csv", sep = ",", header = TRUE)
+View(monthly)
+
+#adding proportion columns
+
+yearly  = yearly %>%
+  mutate(proportion_deaths = deaths/births)
+
+monthly = monthly %>%
+  mutate(proportion_deaths = deaths / births)
+
+#plotting the data
+
+# for years
+
+ggplot(yearly, aes(x= year , y= proportion_deaths, color = clinic )) + geom_line()
+
+#for months
+
+ggplot(monthly, aes(x= as.Date(date) , y= proportion_deaths, group = 1)) + geom_line(color = "red") + 
+  labs( x= "Date", y = "Proportion Deaths") + 
+  scale_x_date(date_labels = "%Y-%m", date_breaks = "6 month")
+
+#adding a threshold
+
+handwashing_start = as.Date("1847-06-01")
+monthly = monthly %>%
+  mutate(handwashing_started = date >= handwashing_start)
+monthly
+
+ggplot(monthly, aes(x= as.Date(date) , y= proportion_deaths, group = 1, color = handwashing_started)) + geom_line() + 
+  labs( x= "Date", y = "Proportion Deaths") + 
+  scale_x_date(date_labels = "%Y-%m", date_breaks = "6 month")
+
+#calculating summary 
+
+monthly_summary = monthly %>%
+  group_by(handwashing_started) %>%
+  summarise(mean_proportion_deaths = mean(proportion_deaths))
+monthly_summary  
 
